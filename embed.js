a7a63161ede1e8f6f384c76cc4c2c5f305c6b8e1
lailahr <lailyseven@gmail.com>
2016-05-12 11:16:28 +0200

    add slash

--- embed.js
+++ embed.js
@@ -1,7 +1,7 @@
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
-    var baseurl = "https://static.code4sa.org/cape-town-fires";
+    var baseurl = "https://static.code4sa.org/cape-town-fires/";
 }
 document.write('<div id="code4sa-cape-town-fires-index"></div>');
 document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
