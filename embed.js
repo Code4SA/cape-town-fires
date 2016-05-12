24c4c5ccad1448b7257a2ea453c0c9003f36d435
lailahr <lailyseven@gmail.com>
2016-05-12 11:12:35 +0200

    update to cape town fires

--- embed.js
+++ embed.js
@@ -1,8 +1,8 @@
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
-    var baseurl = "https://static.code4sa.org/welcome-to-los-angeles/";
+    var baseurl = "https://static.code4sa.org/cape-town-fires";
 }
-document.write('<div id="code4sa-welcome-to-los-angeles-index"></div>');
+document.write('<div id="code4sa-cape-town-fires-index"></div>');
 document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
-document.write("<script>var pymParent = new pym.Parent('code4sa-welcome-to-los-angeles-index', '" + baseurl + "index.html', {});</script>");
+document.write("<script>var pymParent = new pym.Parent('code4sa-cape-town-fires-index', '" + baseurl + "index.html', {});</script>");
